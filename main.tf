a9b9ac67eb64cd5ea826d2ad3d837ab98df0ad2c
muhammadh <muhammad.hamza@xflowresearch.com>
2022-03-12 02:25:43 +0500

    Update code to new version (for practice)

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "sample_vm_1" {
+  ami           = var.ec2_ami_id
+  instance_type = var.ec2_instance_type
+  tags = {
+    Name = var.ec2_instance_name
+  }
+}
+
+resource "aws_instance" "sample_vm_2" {
   ami           = var.ec2_ami_id
   instance_type = var.ec2_instance_type
   tags = {
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "ec2_instance_id" {
-    value = aws_instance.web.id
+    value = aws_instance.sample_vm_1.id
+    description = "The AWS ID of the EC2 instance #1 we created."
+}
+
+output "ec2_instance_id" {
+    value = aws_instance.sample_vm_2.id
+    description = "The AWS ID of the EC2 instance #2 we created."
 }
